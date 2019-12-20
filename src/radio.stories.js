ed59335f3f1b8b54847f3978a1d580a94615f625
Levi Rizki Saputra <levirs565@gmail.com>
2019-12-20 16:21:25 +0700

    add radio

---
+++ src/radio.stories.js
@@ -0,0 +1,26 @@
+import hb from 'handlebars';
+
+export default {
+  title: 'Radio'
+};
+
+import './radio.css';
+
+export const radio = () => {
+  return hb.compile(
+    `<form action="">
+      {{#each option}}
+        <div class="ao-radio">
+          <input type="radio" id="{{this}}" name="choice">
+          <label for="{{this}}">{{this}}</label>
+        </div>
+      {{/each}}
+        <div class="ao-radio">
+          <input type="radio" id="Three" name="choice" disabled>
+          <label for="Three">Three</label>
+        </div>
+    </form>`
+  )({
+    option: ['One', 'Two']
+  });
+};
