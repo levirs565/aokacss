f9850a28094fc889122c383d271426ef7b7eadb3
Levi Rizki Saputra <levirs565@gmail.com>
2019-12-21 09:37:35 +0700

    make check easy

--- src/check.stories.js
+++ src/check.stories.js
@@ -12,16 +12,16 @@ export const check = () => {
       {{#each option}}
         <div class="ao-check">
           <input type="checkbox" id="{{this}}" name="choice">
-          <label for="{{this}}">{{this}}</label>
+          <label for="{{this}}"><span>{{this}}</span></label>
         </div>
       {{/each}}
         <div class="ao-check">
           <input type="checkbox" id="Three" name="choice" disabled>
-          <label for="Three">Three</label>
+          <label for="Three"><span>Three</span></label>
         </div>
         <div class="ao-check">
           <input type="checkbox" id="Four" name="choice" disabled checked>
-          <label for="Four">Four</label>
+          <label for="Four"><span>Four</span></label>
         </div>
     </form>`
   )({
