b02f499c665d22e188cbefd58141ae234aae86e8
Levi Rizki Saputra <levirs565@gmail.com>
2019-12-26 18:36:59 +0700

    improve app bar

--- src/app-bar-top.stories.js
+++ src/app-bar-top.stories.js
@@ -2,14 +2,48 @@ export default {
   title: 'App Bar Top'
 };
 
+import './icon.css';
 import './app-bar-top.css';
 
-export const appBar = () => {
+export const full = () => {
   return `
     <nav class="ao-app-bar-top">
-      <button class="ao-button ao-button-icon in-primary">
+      <button class="icon ao-button ao-button-icon in-primary">
         <i class="ao-icons">menu</i>
       </button>
+      <span class="title">AokaCSS</span>
+      <div class="actions">
+        <button class="ao-button ao-button-icon in-primary">
+          <i class="ao-icons">search</i>
+        </button>
+        <button class="ao-button ao-button-icon in-primary">
+          <i class="ao-icons">more</i>
+        </button>
+      </div>
+    </nav>
+  `;
+};
+
+export const withoutButton = () => {
+  return `
+    <nav class="ao-app-bar-top">
+      <span class="title">AokaCSS</span>
+      <div class="actions">
+        <button class="ao-button ao-button-icon in-primary">
+          <i class="ao-icons">search</i>
+        </button>
+        <button class="ao-button ao-button-icon in-primary">
+          <i class="ao-icons">more</i>
+        </button>
+      </div>
+    </nav>
+  `;
+};
+
+export const withIcon = () => {
+  return `
+    <nav class="ao-app-bar-top">
+      <i class="icon ao-icons">more</i>
       <span class="title">AokaCSS</span>
       <div class="actions">
         <button class="ao-button ao-button-icon in-primary">
