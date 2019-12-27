97bb8554a8e3285dc30141bc8ebcca5c1169d9e4
Levi Rizki Saputra <levirs565@gmail.com>
2019-12-27 08:57:37 +0700

    add typography

---
+++ src/text.stories.js
@@ -0,0 +1,24 @@
+export default {
+  title: 'Text'
+};
+
+import './text.css';
+
+export const text = () =>
+  `
+    <div>
+      <h1 class="ao-heading-1">Heading 1</h1>
+      <h2 class="ao-heading-2">Heading 2</h2>
+      <h3 class="ao-heading-3">Heading 3</h3>
+      <h4 class="ao-heading-4">Heading 4</h4>
+      <h5 class="ao-heading-5">Heading 5</h5>
+      <h6 class="ao-heading-6">Heading 6</h6>
+      <p class="ao-sub-1">Subtitle 1</p>
+      <p class="ao-sub-2">Subtitle 2</p>
+      <p class="ao-body-1">Body 1</p>
+      <p class="ao-body-2">Body 2</p>
+      <p class="ao-button">Button</p>
+      <p class="ao-caption">Caption</p>
+      <p class="ao-overline">Overline</p>
+    </div>
+  `;
