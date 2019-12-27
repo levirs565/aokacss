4206442a8f72f12a68b0a2fa26677d3c51a17b19
Levi Rizki Saputra <levirs565@gmail.com>
2019-12-27 09:52:26 +0700

    make font-size easy

--- util/css-function.js
+++ util/css-function.js
@@ -1,5 +1,11 @@
 module.exports = {
   color(name) {
     return `var(--color-${name})`;
+  },
+  toRem(val) {
+    return `${val * 0.0625}rem`;
+  },
+  letterSpace(number) {
+    return `${number * 0.125}rem`;
   }
 };
