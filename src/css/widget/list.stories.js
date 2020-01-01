274c2a80897fbab88d3a8004b8d95f5db5971e12
Levi Rizki Saputra <levirs565@gmail.com>
2020-01-01 19:49:20 +0700

    improving list

--- src/css/widget/list.stories.js
+++ src/css/widget/list.stories.js
@@ -2,22 +2,23 @@ export default {
   title: 'List'
 };
 
-function genList(cl, line) {
+function genList(cl, line, card) {
   let str = '';
   for (let i = 0; i < 10; i++) {
     str += `
-    <li class="item">
-      <img class="img bg-gray">
-      <div>
-        <p class="text-body-1">${i}</p>
+    <li class="list-item ${card ? 'card col-4' : ''} line-${line}">
+      <img class="${cl} bg-gray">
+      <div class="text">
+        <p class="text-body-1">T${i}</p>
         ${line >= 2 ? `<p class="text-body-2">${i}</p>` : ''}
+        ${line >= 3 ? `<p class="text-body-2">${i}</p>` : ''}
       </div>
     </li>
     `;
   }
 
   return `
-  <ul class="list ${cl} line-${line}">
+  <ul class="list ${card ? 'row' : ' '}">
     ${str}
   </ul>
   `;
@@ -33,3 +34,9 @@ export const twoLineSmallImg = () => genList('img-sm', 2);
 export const twoLineMediumImg = () => genList('img-md', 2);
 export const twoLineLargeImg = () => genList('img-lg', 2);
 export const twoLineExtraLargeImg = () => genList('img-xl', 2);
+export const threeLineList = () => genList('', 3);
+export const threeLineSmallImg = () => genList('img-sm', 3);
+export const threeLineMediumImg = () => genList('img-md', 3);
+export const threeLineLargeImg = () => genList('img-lg', 3);
+export const threeLineExtraLargeImg = () => genList('img-xl', 3);
+export const withCard = () => genList('img-xl', 2, true);
